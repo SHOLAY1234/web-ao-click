f337681a595b9afeef4aa29839441c9e1faa35c5
AO NEXUS LABS <121709547+SHOLAY1234@users.noreply.github.com>
2023-12-12 06:05:33 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,17 +16,55 @@ import {
   getCurrentGame,
   saveClick,
 } from "../lib/clicker-anchor-client";
-import ExternalLink from "@/components/ExternalLink";
 
 const Home: NextPage = () => {
-  // Updated color values with gradients
-  const backgroundGradient = 'linear-gradient(to right, #FFD3B5, #FD6585)'; // Gradient from Apricot to Wild Watermelon
-  const navbarGradient = 'linear-gradient(to right, #36D1DC, #5B86E5)'; // Gradient from Dodger Blue to Light Slate Gray
-  const buttonGradient = 'linear-gradient(to right, #FF6B6B, #FFD166)'; // Gradient from Melon to Pastel Yellow
-  const buttonHoverGradient = 'linear-gradient(to right, #FF8C8C, #FFEA8C)'; // Gradient from Light Salmon to Pastel Yellow
-  const leaderboardGradient = 'linear-gradient(to right, #74ebd5, #ACB6E5)'; // Gradient from Light Green to Lavender
+  const [currentColorSchemeIndex, setCurrentColorSchemeIndex] = useState(0);
 
+  const colorSchemes = [
+    {
+      backgroundGradientBeforeConnect: "linear-gradient(to right, #d1e0f9, #e2e2e2)",
+      backgroundGradient: "linear-gradient(to right, #d1e0f9, #e2e2e2)",
+      navbarGradient: "linear-gradient(to right, #7ea5e0, #7ea5e0)",
+      buttonGradient: "linear-gradient(to right, #ffb2bb, #8dd3ff)",
+      buttonHoverGradient: "linear-gradient(to right, #8dd3ff, #ffb2bb)",
+      leaderboardGradient: "linear-gradient(to right, #adff4e, #9aff9b)",
+    },
+    {
+      backgroundGradientBeforeConnect: "linear-gradient(to right, #bfe5d4, #afd5c2)",
+      backgroundGradient: "linear-gradient(to right, #afd5c2, #bfe5d4)",
+      navbarGradient: "linear-gradient(to right, #39868c, #186174)",
+      buttonGradient: "linear-gradient(to right, #e0d267, #dcb63e)",
+      buttonHoverGradient: "linear-gradient(to right, #dcb63e, #e0d267)",
+      leaderboardGradient: "linear-gradient(to right, #f7ea96, #bdd79c)",
+    },
+    {
+      backgroundGradientBeforeConnect: "linear-gradient(to right, #ffeded, #ffd3d3)",
+      backgroundGradient: "linear-gradient(to right, #ffd3d3, #ffeded)",
+      navbarGradient: "linear-gradient(to right, #ff6666, #ff3333)",
+      buttonGradient: "linear-gradient(to right, #ffaa80, #ff7a59)",
+      buttonHoverGradient: "linear-gradient(to right, #ff7a59, #ffaa80)",
+      leaderboardGradient: "linear-gradient(to right, #ffe680, #ffd66b)",
+    },
+    {
+      backgroundGradientBeforeConnect: "linear-gradient(to right, #d6e2f4, #adc8e6)",
+      backgroundGradient: "linear-gradient(to right, #adc8e6, #d6e2f4)",
+      navbarGradient: "linear-gradient(to right, #4d68b0, #6b82e6)",
+      buttonGradient: "linear-gradient(to right, #ffb971, #ff8a5a)",
+      buttonHoverGradient: "linear-gradient(to right, #ff8a5a, #ffb971)",
+      leaderboardGradient: "linear-gradient(to right, #6ac88e, #4d95b5)",
+    },
+  ];
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentColorSchemeIndex((prevIndex) => (prevIndex + 1) % colorSchemes.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const currentColorScheme = colorSchemes[currentColorSchemeIndex];
+  
   const [clicks, setClicks] = useState(0);
   const [effect, setEffect] = useState(false);
   const [isConnected, setIsConnected] = useState(false);
@@ -65,7 +103,6 @@ const Home: NextPage = () => {
         setGameAccountPublicKey(gameState.gameAccountPublicKey);
         setSolanaExplorerLink(
           `https://explorer.solana.com/address/${gameAccountPublicKey}/anchor-account?cluster=${network}`
-
         );
         setGameError(gameState.errorMessage);
       } else {
@@ -104,27 +141,27 @@ const Home: NextPage = () => {
   }, [wallet, endpoint]);
 
   return (
-    <div className="flex items-center flex-col sm:p-4 p-1" style={{ background: backgroundGradient }}>
+    <div className="flex items-center flex-col sm:p-4 p-1" style={{ background: isConnected ? currentColorScheme.backgroundGradient : currentColorScheme.backgroundGradientBeforeConnect }}>
       <Head>
-        <title>AO CLICK</title>
-        <meta name="title" content="AO CLICK" />
-        <meta name="description" content="AO CLICK is an open-source game being developed to learn and demonstrate techniques for integrating with Solana programs and Solana NFTs." />
+        <title>AMIGOS ODYSSEY CLICK</title>
+        <meta name="title" content="AMIGOS ODYSSEY CLICK" />
+        <meta name="description" content="🌌 Welcome to AO SPACE: The dynamic social frontier of Amigos Odyssey! 🚀 Immerse yourself in a decentralized haven on Solana blockchain devnet, fostering vibrant community engagement with a nominal Sol fee per message. Explore our beta version, a Twitter-style clone, and earn exclusive Discord roles and VIP access to NFT drops by actively participating in AO CLICK. Join the evolving world of Amigos Odyssey, where conversations ignite and innovation meets blockchain for an unparalleled social experience! 🌈💬" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="AO CLICK" />
-        <meta property="og:url" content="https://solana-clicker.netlify.app/" />
-        <meta property="og:image" content="https://solana-clicker.netlify.app/home.png" />
-        <meta property="og:description" content="AO CLICK is an open-source game being developed to learn and demonstrate techniques for integrating with Solana programs and Solana NFTs." />
+        <meta property="og:title" content="AMIGOS ODYSSEY CLICK" />
+        <meta property="og:url" content="https://amigos-odyssey-click.vercel.app/" />
+        <meta property="og:image" content="https://amigos-odyssey-click.vercel.app/" />
+        <meta property="og:description" content="🌌 Welcome to AO SPACE: The dynamic social frontier of Amigos Odyssey! 🚀 Immerse yourself in a decentralized haven on Solana blockchain devnet, fostering vibrant community engagement with a nominal Sol fee per message. Explore our beta version, a Twitter-style clone, and earn exclusive Discord roles and VIP access to NFT drops by actively participating in AO CLICK. Join the evolving world of Amigos Odyssey, where conversations ignite and innovation meets blockchain for an unparalleled social experience! 🌈💬" />
 
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="AO CLICK" />
-        <meta name="twitter:description" content="AO CLICK is an open-source game being developed to learn and demonstrate techniques for integrating with Solana programs and Solana NFTs." />
-        <meta name="twitter:image" content="https://solana-clicker.netlify.app/home.png" />
+        <meta name="twitter:title" content="AMIGOS ODYSSEY CLICK" />
+        <meta name="twitter:description" content="🌌 Welcome to AO SPACE: The dynamic social frontier of Amigos Odyssey! 🚀 Immerse yourself in a decentralized haven on Solana blockchain devnet, fostering vibrant community engagement with a nominal Sol fee per message. Explore our beta version, a Twitter-style clone, and earn exclusive Discord roles and VIP access to NFT drops by actively participating in AO CLICK. Join the evolving world of Amigos Odyssey, where conversations ignite and innovation meets blockchain for an unparalleled social experience! 🌈💬" />
+        <meta name="twitter:image" content="https://amigos-odyssey-click.vercel.app/" />
       </Head>
 
-      <div className="navbar mb-2 text-base-content rounded-full sm:p-4" style={{ background: navbarGradient }}>
+      <div className="navbar mb-2 text-base-content rounded-full sm:p-4" style={{ background: currentColorScheme.navbarGradient }}>
         <div className="flex-1 text-xl font-mono">
-          <img src="/logo.jpg" alt="Logo" className="h-14 w-14 rounded-40px" />
+           <img src="/logo.jpg" alt="Logo" className="h-14 sm:h-14 w-auto rounded-md" />
         </div>
         <div>
           <WalletMultiButton />
@@ -139,7 +176,7 @@ const Home: NextPage = () => {
           <div className="p-4 flex flex-col items-center gap-3">
             <div className="flex flex-col items-center p-2">
               {isGameReady && gameError && (
-                <div className="alert alert-error shadow-lg" style={{ background: buttonGradient }}>
+                <div className="alert alert-error shadow-lg" style={{ background:currentColorScheme. buttonGradient }}>
                   <div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -160,14 +197,13 @@ const Home: NextPage = () => {
               )}
               {isGameReady && (
                 <div
-                onAnimationEnd={() => {
-                  setEffect(false);
-                }}
-                className={effect ? "animate-wiggle" : ""}
-                style={{ color: 'white' }}
-              >
-                {clicks} clicks
-              </div>              
+                  onAnimationEnd={() => {
+                    setEffect(false);
+                  }}
+                  className={effect ? "animate-wiggle" : ""}
+                  style={{ color: 'black', fontWeight: 'bold' }}>
+                  {clicks} clicks
+                </div>
               )}
             </div>
             <button
@@ -177,7 +213,8 @@ const Home: NextPage = () => {
               }}
               className="btn btn-lg text-white border-4 h-36 w-36 rounded-full transform transition-transform hover:scale-105"
               style={{
-                background: buttonGradient,
+                color: 'black', fontWeight: 'bold',
+                background:currentColorScheme. buttonGradient,
                 position: 'relative',
                 overflow: 'hidden',
               }}
@@ -186,26 +223,51 @@ const Home: NextPage = () => {
                 className="absolute top-0 left-0 w-full h-full bg-white opacity-20"
                 style={{
                   transition: 'background 0.3s ease-out',
-                  background: buttonHoverGradient,
+                  background:currentColorScheme. buttonHoverGradient,
+                  color: 'black', fontWeight: 'bold'
                 }}
               ></span>
-              Click Me
+              Click Me 
             </button>
 
             {isGameReady && (
+               <div className="sm:w-3/4">
               <div>
-                View game{" "}
-                <a
-                  className="underline"
-                  href={solanaExplorerLink}
-                  target="_blank"
-                  rel="noreferrer"
-                  style={{ color: buttonGradient }}
-                >
-                  details
-                </a>{" "}
-                on Solana.
+                <p>
+                  <strong>
+                    <a
+                      className="underline"
+                      href="https://twitter.com/amigosodyssey"
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{ color: 'black', fontWeight: 'bold' }}
+                    >
+                      Guide
+                    </a>
+                  </strong>
+                </p>
+                <p style={{ color: 'black', fontWeight: 'bold' }}>
+                  Aim for 500 clicks to unlock exclusive minting access!
+                </p>
+                <p style={{ color: 'black', fontWeight: 'bold' }}>
+                  To participate, simply click the Click Me button. Achieve 500 clicks to qualify for a complimentary minting opportunity.
+                </p>
+                <p style={{ color: 'black', fontWeight: 'bold' }}>
+                  Stay informed and receive timely updates by following us on{" "}
+                  <strong>
+                    <a
+                      className="underline"
+                      href="https://twitter.com/amigosodyssey"
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{ color: 'black', fontWeight: 'bold' }}
+                    >
+                      Twitter
+                    </a>
+                  </strong>
+                </p>
               </div>
+            </div> 
             )}
 
             {!isConnected && (
@@ -226,7 +288,7 @@ const Home: NextPage = () => {
               leaders={leaders}
               walletPublicKeyString={wallet.publicKey.toBase58()}
               clicks={clicks}
-              backgroundGradient={leaderboardGradient}
+              backgroundGradient={currentColorScheme.leaderboardGradient}
             />
           )}
         </div>
